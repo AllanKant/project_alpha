a93e5ec2a4737368421d52f2ac35f4b317cd1af2
AllanKant <146722928+AllanKant@users.noreply.github.com>
2023-10-30 20:57:57 +1300

    Cleaning up the files

--- weight_recorder.py
+++ weight_recorder.py
@@ -7,7 +7,7 @@ root = tk.Tk()
 root.title("Weight Recorder")
 root.geometry("300x100")
 
-label = tk.Label(root, text= "Hi Kav - Enter weight here: ")
+label = tk.Label(root, text= "Enter weight here: ")
 label.pack()
 
 entryfield = tk.Entry(root)
@@ -26,27 +26,15 @@ def on_button_click():
     except ValueError:
         label.config(text="Try Again - Enter a Number)")
     else:
-        if weight != 0:
+        if weight > 50 and weight <90:
             label.config(text="Weight Saved")
             button.config(text= "Saved")
             with open(datafile, "a") as file:
-                file.write(f"\n[{datetime.date.today()},{float(weight)}]")
+                file.write(f"\n{datetime.date.today()},{float(weight)}")
+        else:
+            label.config(text="Try Again - between 50 and 90 please)")
+
 
 button.config(command= on_button_click)
 
-
-
 root.mainloop()
-
-
-
-
-
-
-
-# # weight = float(input())
-
-# datafile = "/Users/allankant/Documents/Code/project_alpha/weight_data.txt"
-# with open(datafile, "a") as file:
-#     file.write(f"\n[{datetime.date.today()},{weight}]")
-
